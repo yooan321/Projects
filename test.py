4777f7d8c30c2ac2c18628df59fad2e9c7af3bc8
yooan321 <yooanean.99@gmail.com>
2025-07-02 20:58:57 +0100

    plot additions

--- test.py
+++ test.py
@@ -4,3 +4,13 @@ df = pd.read_csv('asdfds.csv')
 df_filtered = df[(df['age'] > 30) & (df['income'].notnull())]
 df.groupby('Name').mean()
 square_list = lambda nums: list(map(lambda x: x**2, nums))
+import matplotlib.pyplot as plt
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+plt.title('Sine Wave')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
+import seaborn as sns
+tips = sns.load_dataset('tips')
